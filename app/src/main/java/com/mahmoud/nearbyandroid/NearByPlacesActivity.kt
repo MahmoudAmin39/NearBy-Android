3733c97686e82f07aa9108b54572ac2bb882fb11
Mahmoud Amin <dr.mahmoud39@gmail.com>
2019-10-25 11:16:06 +0200

    - Add Google API client

--- app/src/main/java/com/mahmoud/nearbyandroid/NearByPlacesActivity.kt
+++ app/src/main/java/com/mahmoud/nearbyandroid/NearByPlacesActivity.kt
@@ -2,20 +2,22 @@ package com.mahmoud.nearbyandroid
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.OnSuccessListener
 import com.mahmoud.nearbyandroid.viewmodels.NearByPlacesViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-class NearByPlacesActivity : AppCompatActivity(), OnSuccessListener<Location> {
+class NearByPlacesActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private val viewModel by lazy {
         ViewModelProviders.of(this)[NearByPlacesViewModel::class.java]
@@ -25,9 +27,15 @@ class NearByPlacesActivity : AppCompatActivity(), OnSuccessListener<Location> {
         AlertDialog.Builder(this)
     }
 
-    private val locationProvider by lazy {
-        LocationServices.getFusedLocationProviderClient(this)
+    private val googleApiClient by lazy {
+        GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build();
     }
+
+    private lateinit var locationProvider: FusedLocationProviderClient
 
     companion object {
         const val PERMISSION_LOCATION = 1
@@ -37,16 +45,7 @@ class NearByPlacesActivity : AppCompatActivity(), OnSuccessListener<Location> {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Runtime permission check
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                PERMISSION_LOCATION)
-        } else {
-            // Permission has already been granted
-            getLocation()
-        }
+        locationProvider = LocationServices.getFusedLocationProviderClient(this)
 
         with(viewModel) {
             progressVisibilityState.observe(this@NearByPlacesActivity, Observer { state -> progressBar.visibility = state })
@@ -96,10 +95,45 @@ class NearByPlacesActivity : AppCompatActivity(), OnSuccessListener<Location> {
     }
 
     private fun getLocation() {
-        locationProvider.lastLocation.addOnSuccessListener(this)
+        locationProvider.lastLocation.addOnSuccessListener { location -> viewModel.setLocationSentToServer(location)}
     }
 
-    override fun onSuccess(location: Location?) {
-        viewModel.setLocationSentToServer(location)
+    override fun onResume() {
+        super.onResume()
+        googleApiClient.connect()
     }
+
+    override fun onStop() {
+        if (googleApiClient.isConnected) {
+            googleApiClient.disconnect()
+        }
+        super.onStop()
+    }
+
+    // endregion
+
+    // region GoogleApiClient implement
+
+    override fun onConnected(p0: Bundle?) {
+        // Runtime permission check
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSION_LOCATION)
+        } else {
+            // Permission has already been granted
+            getLocation()
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Log.d("Mahmoud", p0!!.errorMessage)
+    }
+
+    // endregion
 }
